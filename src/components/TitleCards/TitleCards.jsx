0cfbd85601b672941665445fd33c54b9b629dcc7
Alisha-21-cloud <ahmadalishasyed@gmail.com>
2025-07-02 12:54:06 +0530

    Added the different movie categories on the home page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -3,11 +3,11 @@ import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
   
   return (
     <div className='title-cards'>
-      <h2>Popular on Netflix</h2>
+      <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" >
         {cards_data.map((card, index) => {
           return <div className="card" key={index}>
