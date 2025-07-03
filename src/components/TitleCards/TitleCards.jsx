babda13cd85de65b2e90a045b21671407daff20c
Alisha-21-cloud <ahmadalishasyed@gmail.com>
2025-07-03 16:01:48 +0530

    Seted up the api to fetch the movie trailers on clicking the movie title cards && done with the api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './TitleCards.css'
+import { Link } from 'react-router-dom'
 
 const TitleCards = ({ title, category }) => {
   const [apiData, setApiData] = useState([])
@@ -28,10 +29,10 @@ const TitleCards = ({ title, category }) => {
         ) : (
           apiData.map(card => (
             card.backdrop_path && (
-              <div className="card" key={card.id}>
+              <Link to ={`/player/${card.id}`}className="card" key={card.id}>
                 <img src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`} alt={card.original_title} />
                 <p>{card.original_title}</p>
-              </div>
+              </Link>
             )
           ))
         )}
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: ""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZGJlODc0YTQ0NzgyZTliNTU1YTAyYTlhYjAwNzllZSIsIm5iZiI6MTc1MDI1NTM3MC45NTgwMDAyLCJzdWIiOiI2ODUyYzcwYWQ2NTYzYzg5MzM1ODM0ZTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1_exqe_RH38R2syX6RY4GkPJa7pFrTja-qfeBKoeBO4'
+    }
+  }
+
+  useEffect(() => {
+  fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => {
+      if (res.results && res.results.length > 0) {
+        const officialTrailer = res.results.find(
+          (vid) =>
+            vid.type === "Trailer" &&
+            vid.site === "YouTube" &&
+            vid.official === true
+        );
+        setApiData(officialTrailer || res.results[0]);
+      }
+    })
+    .catch(err => console.error(err));
+}, [id]);
+
   return (
     <div className='player'>
-        
+      <img src={back_arrow_icon} alt="Back" onClick={() => {navigate('/')}}/>
+      <iframe
+        width='90%'
+        height='90%'
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title='trailer'
+        frameBorder='0'
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
